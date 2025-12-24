a30151654f9fed0ae68e79fa818ab1c5b4897dde
Ramees Vadakkan <rameesvadakkan@gmail.com>
2025-12-24 22:57:52 +0530

    changed to sqlite

--- techmart/settings.py
+++ techmart/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url 
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,15 +87,11 @@ WSGI_APPLICATION = 'techmart.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'techmart_db',
-        'USER': 'postgres',
-        'PASSWORD': 'pass1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3", conn_max_age=600)
 }
+# DATABASES = {
+#     'default': {      
+
 
 
 
